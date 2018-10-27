662f40f5a6e501c851df9ac01e998e108503e350
Taylor Danielson <danie665@umn.edu>
2018-10-26 22:49:22 -0700

    second commit

---
+++ for-loops.js
@@ -0,0 +1,7 @@
+for (var number = 0; number <= 12; number = number + 2)
+        console.log(number);
+
+var result = 1;
+for (var counter=0; counter<10; counter=counter+1)
+        result = result*2;
+console.log(result);
