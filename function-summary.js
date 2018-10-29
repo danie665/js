59340a39da853d1cb48788eb9285ea9404f86364
Taylor Danielson <danie665@umn.edu>
2018-10-29 11:57:12 -0700

    minimum.js

---
+++ function-summary.js
@@ -0,0 +1,12 @@
+// Create a function value f
+var f = function(a) {
+    console.log(a + 2);
+};
+
+// Declare g to be a function
+function g(a, b) {
+    return a * b * 3.5;
+}
+
+f(1);
+console.log(g(1, 2));
