367d21c0211b25acfdc92f25fab1770a61df1851
Taylor Danielson <danie665@umn.edu>
2019-02-14 15:57:55 -0800

    Update meadowfield.js

--- meadowfield.js
+++ meadowfield.js
@@ -89,6 +89,15 @@ function findRoute(graph, from, to) {
     }
 }
 
-function goalOrientedRobot({place, parcels}, route) {}
+function goalOrientedRobot({place, parcels}, route) {
+    if (route.length == 0) {
+        let parcel = parcels[0];
+        if (parcel.place != place) {
+            route = findRoute(roadGraph, place, parcel.place);
+        } else {
+            route = findRoute(roadGraph, place, parcel.address);
+        }
+    }
+}
 
 runRobot(VillageState.random(), routeRobot, []);
