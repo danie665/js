7acdbc31fccd4d6a4cb3afc67be29fe75d92f644
danie665 <danie665@umn.edu>
2019-02-13 16:01:10 -0800

    Route robot

--- meadowfield.js
+++ meadowfield.js
@@ -42,28 +42,51 @@ class VillageState {
             return new VillageState(destination, parcels);
         }
     }
-    random(parcelCount) {
-        let parcels = [];
-        for (let i = 0; i < parcelCount; i++) {
-            let address = randomPick(Object.keep(roadGraph));
-            let place;
-            do {
-                place = randomPick(Object.keep(roadGraph));
-            } while (place == address);
-            parcels.push({place, address});
-        }
-        return new VillageState("Post Office", parcels);
-    };
 };
 
-let first = new VillageState(
-    "Post Office",
-    [{place: "Post Office", address: "Alice's House"}]
-);
-let next = first.move("Alice's House");
+function randomPick(array) {
+    let choice = Math.floor(Math.random() * array.length);
+    return array[choice];
+}
 
-console.log(next.place);
-console.log(next.parcels);
-console.log(first.place);
+VillageState.random = function(parcelCount = 5) {
+    let parcels = [];
+    for (let i = 0; i < parcelCount; i++) {
+        let address = randomPick(Object.keys(roadGraph));
+        let place;
+        do {
+            place = randomPick(Object.keys(roadGraph));
+        } while (place == address);
+        parcels.push({place, address});
+    }
+    return new VillageState("Post Office", parcels);
+};
 
-runRobot(VillageState.random(5).randomRobot);
+function runRobot(state, robot, memory) {
+    for (let turn = 0;; turn++) {
+        if (state.parcels.length == 0) {
+            console.log(`Done in ${turn} turns`);
+            break;
+        }
+        let action = robot(state, memory);
+        state = state.move(action.direction);
+        memory = action.memory;
+        console.log(`Moved to ${action.direction}`);
+    }
+}
+
+const mailRoute = [
+    "Alice's House", "Cabin", "Alice's House", "Bob's House",
+    "Town Hall", "Daria's House", "Ernie's House",
+    "Grete's House", "Shop", "Grete's House", "Farm",
+    "Marketplace", "Post Office"
+];
+
+function routeRobot(state, memory) {
+    if (memory.length == 0) {
+        memory = mailRoute;
+    }
+    return {direction: memory[0], memory: memory.slice(1)};
+}
+
+runRobot(VillageState.random(), routeRobot, []);
