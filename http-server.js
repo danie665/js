61034e5119181aaf2e04af24d03897a457802180
Taylor Danielson <danie665@umn.edu>
2019-02-20 10:58:32 -0800

    Update http-server.js

--- http-server.js
+++ http-server.js
@@ -1,1 +1,8 @@
-
+const {createServer} = request("http");
+let server = createServer((request, response) => {
+  response.writeHead(200, {"Content-Type"; "test/html"});
+  response.write(`
+    <h1>Hello!</h1>
+    <p>You asked for <code>${request.url}</code></p>`);
+  response.end();
+});
