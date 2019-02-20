e645dd1c2b2e0bf72e1498a371595c22458f003d
Taylor Danielson <danie665@umn.edu>
2019-02-20 11:05:45 -0800

    Update http-server.js

--- http-server.js
+++ http-server.js
@@ -6,3 +6,5 @@ let server = createServer((request, response) => {
     <p>You asked for <code>${request.url}</code></p>`);
   response.end();
 });
+server.listen(8000);
+console.log("Listening! (port 8000)");
