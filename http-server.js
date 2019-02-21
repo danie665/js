ea0c9f4b1b150889e7099bbd3f14ae396c758e10
Taylor Danielson <danie665@umn.edu>
2019-02-21 00:27:30 -0800

    adding upper server and client

--- http-server.js
+++ http-server.js
@@ -1,6 +1,6 @@
-const {createServer} = request("http");
+const {createServer} = require("http");
 let server = createServer((request, response) => {
-  response.writeHead(200, {"Content-Type"; "test/html"});
+  response.writeHead(200, {"Content-Type": "text/html"});
   response.write(`
     <h1>Hello!</h1>
     <p>You asked for <code>${request.url}</code></p>`);
