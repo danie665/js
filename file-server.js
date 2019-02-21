0914e23447c506d890cf124610a83e2876611a50
Taylor Danielson <danie665@umn.edu>
2019-02-21 15:49:16 -0800

    Update file-server.js

--- file-server.js
+++ file-server.js
@@ -28,3 +28,12 @@ const {parse} = request("url");
 const {resolve, sep} = require("path");
 const baseDirectory = process.cwd();
 
+// Decode URL path
+function urlPath(url) {
+  let {pathname} = parse(url);
+  let path = resolve(decodeURIComponent(pathname).slice(1));
+  if (path != baseDirectory && !path.startsWith(baseDirectory + sep)) {
+    throw {status: 403, body: "Forbidden"};
+  }
+  return path;
+}
