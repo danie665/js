f28bfb06b26770f7dcd66f9a593345a07d07c8ee
Taylor Danielson <danie665@umn.edu>
2019-02-26 13:49:26 -0800

    Update file-server.js

--- file-server.js
+++ file-server.js
@@ -58,3 +58,19 @@ methods.GET = async function(request) {
             type: mime.getType(path)};
   }
 };
+
+const {rmdir, unlink} = require("fs").promises;
+
+methods.DELETE = async function(request) {
+  let path = urlPath(request.url);
+  let stats;
+  try {
+    stats = await stat(path);
+  } catch(error) {
+    if (error.code != "ENONET") throw error;
+    else return {status: 204};
+  }
+  if (stats.isDirectory()) await rmdir(path);
+  else await unlink(path);
+  return {status: 204}
+};
