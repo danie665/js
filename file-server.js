1d3d8e0aea94c843d06a749986e39a79c1bf23ea
Taylor Danielson <danie665@umn.edu>
2019-02-26 13:24:46 -0800

    Update file-server.js

--- file-server.js
+++ file-server.js
@@ -42,4 +42,19 @@ const {createReadStream} = require("fs");
 const {stat, readdir} = require("fs").promises;
 const mime = require("mime");
 
-methods.GET = async function(request {}
+methods.GET = async function(request) {
+  let path = urlPath(request.url);
+  let stats;
+  try {
+    stats = await stat(path);
+  } catch (error) {
+    if (error.code != "ENONET") throw error;
+    else return {status: 404, body: "File not found"};
+  }
+  if (stats.isDirectory()) {
+    return {body: (await readdir(path)).join("\n")};
+  } else {
+    return {body: createReadStream(path),
+            type: mime.getType(path)};
+  }
+};
